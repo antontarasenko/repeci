879774796dfc5404eaeb2f2d42f2d5422afce7e3
Anton Tarasenko <antontarasenko@gmail.com>
2014-09-03 07:45:56 +0300

    Setup.py added.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='repeci',
+    version='0.1a1',
+    packages=['repeci', 'repeci.tests'],
+    url='',
+    license='',
+    author='Anton Tarasenko',
+    author_email='antontarasenko@gmail.com',
+    description=''
+)
